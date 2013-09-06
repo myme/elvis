db09fe804b6570ac24943bb1ee209518731e3419
Martin Øinæs Myrseth <myrseth@gmail.com>
2013-09-06 02:23:12 +0200

    Remove buster.js

--- buster.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var pkg = JSON.parse(fs.readFileSync('package.json'));
-
-exports.tests = {
-  environment: 'browser',
-  sources: [ 'dist/' + pkg.name + '.js' ],
-  tests: [ 'dist/**-test.js' ]
-};
